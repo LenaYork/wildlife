f86bb0feb57b36ba3d7b46c05bb546ec33740086
lenayork <lenayork@tut.by>
2024-03-20 23:24:14 +0100

    feat: add mobile menu smooth closing when clicking links

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const modalMenu = document.querySelector(".modal-container");
 const burgerMenu = document.querySelector("#burger-menu");
 const closeIcon = document.querySelector("#close-icon");
+
+const blogLink = document.querySelector("#mobile-blog");
+const contactsLink = document.querySelector("#mobile-contacts");
+
 burgerMenu.addEventListener("click", () => {
     modalMenu.classList.add("visible");
-    
+
 });
 
-closeIcon.addEventListener("click", () => {
+const closeModal = () => {
     modalMenu.classList.remove("visible");
+}
+
+[closeIcon, blogLink, contactsLink].forEach(link => {
+    link.addEventListener("click", closeModal);
 });
