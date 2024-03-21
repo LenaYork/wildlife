258183079154ef24d1e54117008c39089d697c08
lenayork <lenayork@tut.by>
2024-03-21 21:25:42 +0100

    feat: add email validation

--- main.js
+++ main.js
@@ -17,3 +17,29 @@ const closeModal = () => {
 [closeIcon, blogLink, contactsLink].forEach(link => {
     link.addEventListener("click", closeModal);
 });
+
+const subscribeButton = document.querySelector(".notified-input-button");
+
+subscribeButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const email = document.querySelector(".notified-input").value;
+    if (isValidEmail(email)) {
+        location.reload();
+    } else if (email) { showPopup() };
+});
+
+function isValidEmail(email) {
+    const emailRegex = /\S+@\S+\.\S+/;
+    return emailRegex.test(email);
+}
+
+function showPopup() {
+    document.getElementById("popup").style.opacity = 1;
+    setTimeout(() => {
+        document.getElementById("popup").style.opacity = 0;
+    }, 3000);
+}
+
+// function hidePopup() {
+//     document.getElementById("popup").style.opacity = 0;
+// }
